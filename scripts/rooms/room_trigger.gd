03040378be0247b54765f64f82c81f5c34058033
MidnightBlue <113545024+R1fty0@users.noreply.github.com>
2026-05-12 22:58:51 -0700

    imported state machine scripts, made room trigger script

---
+++ scripts/rooms/room_trigger.gd
@@ -0,0 +1,12 @@
+extends Area3D
+class_name RoomTrigger
+
+signal triggered
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		print("Player entered/exited the room")
+		triggered.emit()
---
+++ scripts/state_machine/state.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name State
+
+@warning_ignore("unused_signal")
+signal transitioned 
+
+func enter_state():
+	pass
+
+func exit_state():
+	pass
+	
+func state_process(_delta: float):
+	pass
+	
+func state_physics_process(_delta: float):
+	pass
